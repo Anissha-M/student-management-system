7c8d2847d1eeaa85369f5e09117e8ffb14e92e15
Anissha M <anissha1608@gmail.com>
2026-07-08 12:08:04 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,8 @@
--- Step 1: Create Database
 CREATE DATABASE student_management;
 
--- Step 2: Use the database
 USE student_management;
 
--- Step 3: Create the student table
+
 CREATE TABLE student (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
